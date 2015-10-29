7a50d3a6e6a984ea9143a579b6500057b48a81f6
Marcus Stade <marcus@stade.se>
2015-10-29 18:16:13 +0100

    Remove used hooks

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ module.exports = {
     js: [
       "d3.min.js",
       "book.js"
-    ],
-
-    html: {
-      "html:start": function() {
-          return "<!-- Start book "+this.options.title+" -->"
-      },
-      "html:end": function() {
-          return "<!-- End of book "+this.options.title+" -->"
-      },
-
-      "head:start": "<!-- head:start -->",
-      "head:end": "<!-- head:end -->",
-
-      "body:start": "<!-- body:start -->",
-      "body:end": "<!-- body:end -->"
-    },
+    ]
   },
 
   hooks: {
